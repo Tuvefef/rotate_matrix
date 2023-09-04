6115ccd7f68340ae2b5ee569375935028d61af40
Tuvefef <119376758+Tuvefef@users.noreply.github.com>
2023-09-04 09:09:29 -0600

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ float pi = 3.1415927;
 float a_0;
 float a_1;
 float mat_[2][2];
-int var_ = 2;
+int VAR = 2;
 
 #include "mat2.hpp"
 
@@ -16,8 +16,8 @@ int main(){
     a_1 = a_0 * pi / 180;
     mat_f();
     std::cout << "matriz de rotacion" << std::endl;
-    for(int i_1 = 0; i_1 < var_; i_1++){
-        for(int i_2 = 0; i_2 < var_; i_2++){
+    for(int i_1 = 0; i_1 < VAR; i_1++){
+        for(int i_2 = 0; i_2 < VAR; i_2++){
             std::cout << mat_[i_1][i_2] << std::endl;
         }
         std::cout << std::endl;
